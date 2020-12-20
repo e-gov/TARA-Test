d62037f342f96fb753083490d25b4633addb10d9
Margus Härm <margus.harm@testijad.ee>
2020-12-20 14:47:42 +0200

    TARA2-76 consent confirm tests added

--- src/test/groovy/ee/ria/tara/AuthConsentConfirmSpec.groovy
+++ src/test/groovy/ee/ria/tara/AuthConsentConfirmSpec.groovy
@@ -80,7 +80,9 @@ class AuthConsentConfirmSpec extends TaraSpecification {
         def map1 = Utils.setParameter(cookiesMap, "SESSION", flow.sessionId)
         HashMap<String, String> paramsMap = (HashMap) Collections.emptyMap()
         def map2 = Utils.setParameter(paramsMap, paramName, paramValue)
-        Response response = Requests.getRequestWithCookiesAndParams(flow, flow.loginService.fullConsentUrl, cookiesMap, paramsMap, Collections.emptyMap())
+        HashMap<String, String> additionalParamsMap = (HashMap) Collections.emptyMap()
+        def map3 = Utils.setParameter(additionalParamsMap, additionalParamName, additionalParamValue)
+        Response response = Requests.getRequestWithCookiesAndParams(flow, flow.loginService.fullConsentUrl, cookiesMap, paramsMap, additionalParamsMap)
 
         assertEquals("Correct HTTP status code is returned", statusCode, response.statusCode())
         // TODO "application/json;charset=UTF-8"
@@ -94,6 +96,79 @@ class AuthConsentConfirmSpec extends TaraSpecification {
         "consent_challenge" | RandomStringUtils.random(51, true, true) | _                   | _                    | "Too long consent_challenge"                               || 400        || "authConsent.consentChallenge: size must be between 0 and 50"
         "consent_challenge" | RandomStringUtils.random(50, true, true) | _                   | _                    | "Correct consent_challenge length"                         || 400        || "a'la consent with ID not found"
         "consent_challenge" | "342%26abz"                              | _                   | _                    | "Invalid symbols in the consent_challenge parameter value" || 400        || "authConsent.consentChallenge: only characters and numbers allowed"
+        "consent_challenge" | "ABCD1234"                               | "consent_challenge" | "1234abc"            | "Multiple consent_challenge parameters"                    || 400        || "Multiple request parameters with the same name not allowed"
+    }
+
+    @Unroll
+    @Feature("USER_CONSENT_CONFIRM_ENDPOINT")
+    def "Confirm consent with authentication results. Invalid session ID"() {
+        expect:
+        HashMap<String, String> cookiesMap = (HashMap) Collections.emptyMap()
+        def map1 = Utils.setParameter(cookiesMap, "SESSION", "1234567")
+        HashMap<String, String> paramsMap = (HashMap) Collections.emptyMap()
+        def map2 = Utils.setParameter(paramsMap, "consent_given", true)
+        Response response = Requests.postRequestWithCookiesAndParams(flow, flow.loginService.fullConsenConfirmUrl, cookiesMap, paramsMap, Collections.emptyMap())
+        assertEquals("Correct HTTP status code is returned", 400, response.statusCode())
+        // TODO "application/json;charset=UTF-8"
+        assertEquals("Correct Content-Type is returned", "application/json", response.getContentType())
+        assertEquals("Correct error message is returned", "Teie sessiooni ei leitud! Sessioon aegus või on küpsiste kasutamine Teie brauseris piiratud.", response.body().jsonPath().get("message"))
+    }
+
+    @Ignore // TARA2-76
+    @Unroll
+    @Feature("USER_CONSENT_CONFIRM_ENDPOINT")
+    def "Confirm consent with authentication results. Invalid method get"() {
+        expect:
+        HashMap<String, String> cookiesMap = (HashMap) Collections.emptyMap()
+        def map1 = Utils.setParameter(cookiesMap, "SESSION", "1234567")
+        HashMap<String, String> paramsMap = (HashMap) Collections.emptyMap()
+        def map2 = Utils.setParameter(paramsMap, "consent_given", true)
+        Response response = Requests.getRequestWithCookiesAndParams(flow, flow.loginService.fullConsenConfirmUrl, cookiesMap, paramsMap, Collections.emptyMap())
+        assertEquals("Correct HTTP status code is returned", 400, response.statusCode())
+        // TODO "application/json;charset=UTF-8"
+        assertEquals("Correct Content-Type is returned", "application/json", response.getContentType())
+        assertEquals("Correct error message is returned", "Request method 'GET' not supported", response.body().jsonPath().get("message"))
+    }
+
+    @Unroll
+    @Feature("USER_CONSENT_CONFIRM_ENDPOINT")
+    def "Confirm consent with authentication results. Missing session ID"() {
+        expect:
+        HashMap<String, String> paramsMap = (HashMap) Collections.emptyMap()
+        def map2 = Utils.setParameter(paramsMap, "consent_given", true)
+        Response response = Requests.postRequestWithCookiesAndParams(flow, flow.loginService.fullConsenConfirmUrl, Collections.emptyMap(), paramsMap, Collections.emptyMap())
+        assertEquals("Correct HTTP status code is returned", 400, response.statusCode())
+        // TODO "application/json;charset=UTF-8"
+        assertEquals("Correct Content-Type is returned", "application/json", response.getContentType())
+        assertEquals("Correct error message is returned", "Teie sessiooni ei leitud! Sessioon aegus või on küpsiste kasutamine Teie brauseris piiratud.", response.body().jsonPath().get("message"))
+    }
+
+    @Unroll
+    @Feature("USER_CONSENT_CONFIRM_ENDPOINT")
+    def "Confirm consent with authentication results. #label"() {
+        expect:
+        Response initResponse = Steps.authWithMobileID(flow)
+        Response consentResponse = Steps.followRedirectWithSessionId(flow, initResponse)
+        assertEquals("Correct HTTP status code is returned", 302, consentResponse.statusCode())
+        HashMap<String, String> cookiesMap = (HashMap) Collections.emptyMap()
+        def map1 = Utils.setParameter(cookiesMap, "SESSION", flow.sessionId)
+        HashMap<String, String> paramsMap = (HashMap) Collections.emptyMap()
+        def map2 = Utils.setParameter(paramsMap, paramName, paramValue)
+        HashMap<String, String> additionalParamsMap = (HashMap) Collections.emptyMap()
+        def map3 = Utils.setParameter(additionalParamsMap, additionalParamName, additionalParamValue)
+        Response response = Requests.postRequestWithCookiesAndParams(flow, flow.loginService.fullConsenConfirmUrl, cookiesMap, paramsMap, additionalParamsMap)
+        assertEquals("Correct HTTP status code is returned", statusCode, response.statusCode())
+        // TODO "application/json;charset=UTF-8"
+        assertEquals("Correct Content-Type is returned", "application/json", response.getContentType())
+        assertEquals("Correct error message is returned", errorMessage, response.body().jsonPath().get("message"))
+
+        where:
+        paramName       | paramValue | additionalParamName | additionalParamValue | label                                 || statusCode || errorMessage
+        _               | _          | _                   | _                    | "Missing parameter consent_given"     || 400        || "Required String parameter 'consent_given' is not present"
+        "consent_given" | _          | _                   | _                    | "Empty parameter consent_given value" || 400        || "authConsentConfirm.consentGiven: supported values are: 'true', 'false'"
+        "consent_given" | "abc123"   | _                   | _                    | "Invalid consent_given value"         || 400        || "authConsentConfirm.consentGiven: supported values are: 'true', 'false'"
+        "consent_given" | "false"    | _                   | _                    | "Session does not exists"             || 400        || "Ebakorrektne päring. Vale sessiooni staatus."
+        "consent_given" | "false"    | "consent_given"     | "true"               | "Multiple consent_given parameters"   || 400        || "Multiple request parameters with the same name not allowed"
     }
 
 }
