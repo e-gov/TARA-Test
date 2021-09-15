8976f70ee5c79215b92ab17cf4887bb0dbc3bd1e
Markus Kivisalu <markus.kivisalu@gmail.com>
2021-09-15 17:46:55 +0300

    AUT-607 adjusted pom.xml and SpockConfig

--- src/test/resources/SpockConfig.groovy
+++ src/test/resources/SpockConfig.groovy
@@ -1,8 +1,13 @@
+import org.spockframework.runtime.model.parallel.ExecutionMode
+import spock.config.ParallelConfiguration
+
 runner {
     filterStackTrace false
     optimizeRunOrder true
     parallel {
         enabled true
+        defaultSpecificationExecutionMode = ExecutionMode.CONCURRENT
+        defaultExecutionMode = ExecutionMode.SAME_THREAD
         dynamic(2.0)
     }
 }
